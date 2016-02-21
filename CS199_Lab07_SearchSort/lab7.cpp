c5a1d90e80c81df0f62a3235294ca8821ec6cc6e
Giovanni Rosati <giovanni.rosati@gmail.com>
2016-02-21 14:50:44 -0800

    linear search time trial complete

--- CS199_Lab07_SearchSort/lab7.cpp
+++ CS199_Lab07_SearchSort/lab7.cpp
@@ -8,7 +8,9 @@
 
 //libraries
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
+#include <string>
 #include <time.h>	//added so srand could be used with time
 
 //header files
@@ -17,22 +19,27 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::setprecision;
+using std::string;
+using std::to_string;
 
 //function prototypes
-void binarySearch(int arr[], int size, int value);
-void linearSearch(int arr[], int size, int value);
+bool binarySearch(int arr[], int size, int searchValue);
+bool linearSearch(int arr[], int size, int searchValue);
 void mySort(int arr[], int size);
+const int size = 1000;
 
 //main
 int main()
 {
 	srand(time(0));	//seed random number generator
-	//create two identical arrays with 1,000 ints, each position 0-999
-	int arrayOne[1000];
-	int arrayTwo[1000];
-	for (int i = 0; i < 1000; i++)
+	int searches = 0;
+	//create two identical arrays with 1,000 ints, each position has random value of 0-999
+	int arrayOne[size];
+	int arrayTwo[size];
+	for (int i = 0; i < size; i++)
 	{
-		int value = rand() % 1000;  //generates 0 to 999
+		int value = rand() % size;  //generates 0 to 999
 		arrayOne[i] = value;
 		arrayTwo[i] = value;
 	}
@@ -42,17 +49,73 @@ int main()
 	//	Sorting and 100,000,000 binary searches using random numner 0-999
 	//Display the two times to the console with clear descriptions
 
-	int searchValue = rand() % 1000;  //generates 0 to 999
-	cout << "Search value is: " << searchValue << endl;
-	cin.get();
+	//int searchValue = rand() % 1000;  //generates 0 to 999
+	//cout << "Search value is: " << searchValue << endl;
+	//cin.get();
 
 	//However the double difftime(time_t t1, time_t t0) function returns the difference t1 - t0 expressed in seconds as a double
 
 	//linear search time trial
-	void linearSearch(int arr[], int size, int value);
+	cout << "Beginning linear search time trial." << endl;
+	cout << "enter number of searches to perform in the time trial and press enter to start. " << endl;
+	cin >> searches;
+
+	double linearTimeStart = time(0);
+	for (int i = 1; i < searches; i++)
+	{
+		int searchValue = rand() % 1000;  //generates 0 to 999
+		linearSearch(arrayOne, size, searchValue);
+	}
+	double linearTimeEnd = time(0);
+	double timeElapsed = linearTimeEnd - linearTimeStart;
+	cout << "Completed linear search time trial." << endl;
+
+	string searchesCommas = to_string(searches);
+	int insertPosition = searchesCommas.length() - 3;
+	while (insertPosition > 0)
+	{
+		searchesCommas.insert(insertPosition, ",");
+		insertPosition -= 3;
+	}
+
+	cout << "Seconds to complete " <<  searchesCommas << " linear searches was: " << timeElapsed << endl;
+	cin.ignore();
+	cin.get();
 
 	//binary search time trial
-	void binarySearch(int arr[], int size, int value);
+
+
+
+
+
+	//void binarySearch(int arr[], int size, int value);
 
 	return 0;
+}
+
+bool linearSearch(int arr[], int size, int searchValue)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == searchValue)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool linearSearch(int arr[], int size, int searchValue) 
+{
+	//step one is to sort the array
+	mySort(arr, size);
+	//step two, binary search
+
+
+}
+
+void mySort(int arr[], int size);
+{
+
+
 }
